db55c1510f8a22714353e5b7bb0119e7a90ddca2
JDAV-5 <javierdavila071@gmail.com>
2026-05-07 20:00:37 -0600

    Corrigiendo problemas de proporcion

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 console.log("Landing Page lista");
 
+const links = document.querySelectorAll('.nav-links a');
 
+links.forEach(link => {
+
+    link.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+
+        const targetSection = document.querySelector(targetId);
+
+        const offset = 80; // altura navbar
+
+        const sectionPosition =
+            targetSection.getBoundingClientRect().top + window.pageYOffset - offset;
+
+        window.scrollTo({
+            top: sectionPosition,
+            behavior: 'smooth'
+        });
+
+    });
+
+});
 
 // Get the current URL
 //var currentUrl = window.location.href;
